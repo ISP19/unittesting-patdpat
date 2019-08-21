660dba40ca6055f13b2c1fda8128dfb61a354f67
sidtipat.k <s.kietchai@outlook.com>
2019-08-21 19:55:05 +0700

    add some doctest to listutil

--- listutil.py
+++ listutil.py
@@ -8,12 +8,24 @@ def unique(list):
         a new list containing only distinct elements from list
 
     Examples:
-    >>> unique([5])
+    >>> unique([5,5]) #test int
     [5]
-    >>> unique(["b","a","a","b","b","b","a","a"])
+    >>> unique(["b","a","a","b","b","b","a","a"]) #test string
     ['b', 'a']
-    >>> unique([])
+    >>> unique([True, False,True]) #test booline
+    [True, False]
+    >>> unique([0.53,0.53]) #test float
+    [0.53]
+    >>> unique([[5],[5]]) #test list and check whether it's do  do a recursive scan or not
+    [[5]]
+    >>> unique([]) #test empty list
     []
+    >>> unique(["b","a","a",0.53,0.53,5,5,True,True,[5]]) #test alltype at once
+    ['b', 'a', 0.53, 5, True, [5]]
+    >>> unique([777]) #test single element
+    [777]
+
+
     """
     result = []
     for i in list:
