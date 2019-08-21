12475146cea79c79efc8212749ff4ac44f5b6976
sidtipat.k <s.kietchai@outlook.com>
2019-08-21 13:59:34 +0700

    add some code into fraction.py

--- fraction.py
+++ fraction.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Fraction:
     """A fraction with a numerator and denominator and arithmetic operations.
 
@@ -12,15 +15,57 @@ class Fraction:
         """Initialize a new fraction with the given numerator
            and denominator (default 1).
         """
-        self.numerator = numerator
-        self.denominator = denominator
+        if type(numerator) is not int:  # prevent string or float in numerator
+            raise TypeError("Type of numberator must be int")
+        if type(denominator) is not int:  # prevent string or float in denominator
+            raise TypeError("Type of denominator must be int")
+        if denominator == 0:  # prevent division by zero
+            raise ValueError(
+                "Division by zero is undefined in ordinary arithmetic.")
+        else:
+            self.numerator = numerator
+            self.denominator = denominator
+            self.make_simplest_form()
+
+    def make_simplest_form(self):
+        """method to convert any fraction to simplest form
+        theory of using gcd to simplest form from https://www.chilimath.com/lessons/introductory-algebra/simplifying-fractions/            
+        """
+        gcd = math.gcd(self.numerator, self.denominator)
+        self.numerator = self.numerator/gcd
+        self.denominator = self.denominator/gcd
+
+    def make_decimal(self):
+        """method to convert fraction to decimal form
+        """
+        decimal_form = self.numerator/self.denominator
+        return decimal_form
+
+    def check_negative(self):
+        """method to check the value return true if it's value is negative
+        """
+        return True if self.make_decimal() < 0 else False:
+
+    def check_negative(self):
+        """method to check the value return true if it's value is positive
+        """
+        return True if self.make_decimal() > 0 else False:
+
+    def check_zero(self):
+        """method to check the value return true if it's value is zero
+        """
+        return True if self.make_decimal() == 0 else False:
+
+    def __str__(self):
+        """Return string form of fraction
+        """
 
     def __add__(self, frac):
         """Return the sum of two fractions as a new fraction.
            Use the standard formula  a/b + c/d = (ad+bc)/(b*d)
         """
-        # from given formula a/b + c/d = (ad+bc)/(b*d) these list are for variable that require
-        # to put in to formula
+        # from given formula a/b + c/d = (ad+bc)/(b*d) these are variables that require
+        # to put into formula
         a = int(self.numerator)
         b = int(self.denominator)
         c = int(frac.numerator)
@@ -38,9 +83,8 @@ class Fraction:
         """Return the  product of multiplication of two fraction as a new fraction.
         """
         # formula for calculate fraction multiplication is a/b * c/d = ac/bd
-
-
-
+        # from above formula a/b * c/d = ac/bd these are variables that require
+        # to put into formula
 
     def __eq__(self, frac):
         """Two fractions are equal if they have the same value.
